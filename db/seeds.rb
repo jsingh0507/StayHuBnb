c8d7fe0556c4931c29273942d84f138fd8a7bb94
Jaspreet Singh <99jasingh@gmail.com>
2024-05-23 18:18:20 -0400

    added Authentication methods for user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+    # Create one user with an easy to remember username, email, and password:
+    User.create!(
+      full_name: 'Demo Lition', 
+      email: 'demo@user.io', 
+      password: 'password'
+    )
+  
+    # More users
+    User.create!(
+      full_name: 'John Doe', 
+      email: 'dodo123@user.io', 
+      password: 'qwerty123'
+    )
+
+    User.create!(
+      full_name: 'Jaspreet Singh', 
+      email: 'jaspreet123@gmail.io', 
+      password: 'password123'
+    )
+  
+    puts "Done!"
+  end
