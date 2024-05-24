78931f05ae2659e190389392fbb5a840d541c4b7
Jaspreet Singh <99jasingh@gmail.com>
2024-05-24 16:11:52 -0400

    Auth Backend done

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,59 +1,27 @@
 class Api::SessionsController < ApplicationController
-  # def show
-  #   if current_user
-  #     @user = current_user
-  #     render 'api/users/show'
-  #   else
-  #     render json: { user: nil }
-  #   end
-  # end
-
-  # def create
-  #   @user = User.find_by_credentials(params[:email], params[:password])
-
-  #   if @user
-  #     login!(@user)
-  #     render 'api/users/show'
-  #   else
-  #     render json: { errors: ['The provided credentials were invalid.'] }, 
-  #       status: :unauthorized
-  #   end
-  # end
-
-  # def destroy
-  #   logout!
-  #   render json: { message: 'success' }
-  # end
-
   def show
     if current_user
-      render json: { user: current_user }
+      @user = current_user
+      render 'api/users/show'
     else
       render json: { user: nil }
     end
   end
 
   def create
-    # Assume credentials are passed in as JSON in the request body
-    email = params[:email]
-    password = params[:password]
-    
-    @user = User.find_by_credentials(email, password)
+    @user = User.find_by_credentials(params[:email], params[:password])
 
     if @user
       login!(@user)
-      render json: { user: @user }
+      render 'api/users/show'
     else
-      render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
     end
   end
 
   def destroy
-    if current_user
-      logout!
-      render json: { message: 'success' }
-    else
-      render json: { message: 'No user logged in' }
-    end
+    logout!
+    render json: { message: 'success' }
   end
 end
